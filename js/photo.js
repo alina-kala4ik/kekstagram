c269e3ddc9d4bc5eb28a85f4314077c6990b6472
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-03-22 12:45:16 +0300

    реализована возможность изменения масштаба изображения

--- js/photo.js
+++ js/photo.js
@@ -8,6 +8,9 @@
   let editor = document.querySelector('.img-upload__overlay');
   let photo = editor.querySelector('.img-upload__preview img');
   let buttonCloseEditor = editor.querySelector('#upload-cancel');
+  let controlSmaller = editor.querySelector('.scale__control--smaller');
+  let controlBigger = editor.querySelector('.scale__control--bigger');
+  let controlValue = editor.querySelector('.scale__control--value');
 
   function pullPhoto(fileChooser, preview) {
     let file = fileChooser.files[0];
@@ -32,6 +35,37 @@
   };
 
 
+  function imageResize(way) {
+    let valueNow = parseInt(controlValue.getAttribute('value'));
+    if ( (way === 'decrease' && valueNow <= 25) || (way === 'increase' && valueNow >= 100) ) {
+      return false;
+    } else {
+      let newValue = (way === 'decrease') ? (valueNow - 25) : (valueNow + 25);
+      controlValue.setAttribute('value', (newValue + '%'));
+      photo.style.transform = `scale(${newValue / 100})`;
+    }
+  };
+
+
+  // function imageResize(way) {
+  //   let valueNow = parseInt(controlValue.getAttribute('value'));
+  //   let newValue = valueNow;
+  //   if (valueNow === 25 || valueNow === 100) {
+  //     return false;
+  //   } else {
+  //     newValue = (way === 'decrease') ? (valueNow - 25) : (valueNow + 25)
+  //   }
+  //   if (way === 'decrease') {
+  //     valueNow === 25 ? false : newValue = valueNow - 25;
+  //   } else {
+  //     valueNow === 100 ? false : newValue = valueNow + 25;
+  //   }
+  //   controlValue.setAttribute('value', (newValue + '%'));
+  //   photo.style.transform = `scale(${newValue / 100})`;
+  //   console.log(newValue)
+  // };
+
+
   function openEditor() {
     pullPhoto(uploadFile, photo);
     editor.classList.remove('hidden');
@@ -41,6 +75,13 @@
     document.body.addEventListener('keydown', evt => {
       window.util.isEscEvent(evt, closeEditor);
     })
+
+    controlSmaller.addEventListener('click', () => {
+      imageResize('decrease');
+    });
+    controlBigger.addEventListener('click', () => {
+      imageResize('increase');
+    });
   };
 
   uploadFile.addEventListener('change', openEditor);
